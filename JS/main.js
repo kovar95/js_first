ddbe874158fd43098106ff06190b9bdd6883db4e
Stefan Kovarbasic <kovar.zmaj@gmail.com>
2019-11-22 13:25:44 +0100

    First JS homework

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+
+// TASK 1
+
+var Peter_money = "3500$";
+var Jim_money = "5000$";
+console.log (Peter_money > Jim_money);
+
+// there is option working only with numbers
+
+// var Peter_money = 3500;
+// var Jim_money = 5000;
+// console.log (Peter_money > Jim_money);
+
+// TASK 2
+
+var John_age = 38;
+var Mike_age = (38+8)/2 -8;
+console.log("Mike is " + Mike_age + " years old.");
+
+// TASK 3
+
+var firs_flock = 78;
+var second_flock = 57;
+firs_flock -= 12;
+second_flock += 12;
+second_flock -= 4;
+console.log(firs_flock >= second_flock);
